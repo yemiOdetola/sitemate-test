f7cf0e2fd58e90691a051a0463871580ae823d6c
Odetola Azeez Opeyemi <yemiotola@gmail.com>
2024-09-07 13:23:59 +0100

    cleanup frontend files

--- frontend/src/api/issues.ts
+++ frontend/src/api/issues.ts
@@ -1,7 +1,6 @@
-// src/services/issueService.ts
 import { Issue } from "@/types";
 import axios from "axios";
-export const API_URL = "http://localhost:3000";
+const API_URL = "http://localhost:3000";
 
 export const getIssues = async (): Promise<Issue[]> => {
   const response = await axios.get(`${API_URL}/issues`);
